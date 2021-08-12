a561e447a7be21743116580cc930c7bfbbdca400
Kamila Bandeira <kamila_bandeira@hotmail.com>
2021-08-12 15:07:27 -0300

    if/AND(&&)/OR(||)/escopo

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 8 - Condicionais 2");
+
+            int idadeJoao = 18;
+            int quantidadePessoas = 2;
+            bool acompanhado = quantidadePessoas >= 2;
+            //bool acompanhado = false;
+
+            if (idadeJoao >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 9 - Escopo");
+
+            int idadeJoao = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João esta acompanahdo";
+            }
+            else
+            {
+                mensagemAdicional = "João não esta acompanahdo";
+            }
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
